862bafd608fbbb0145db6fae38683b8255d59b1c
sandilemp <sandilephensulo@gmail.com>
2022-07-27 15:10:54 +0200

    Added health check endpoint for alb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser')
 AWS.config.update({ region: 'eu-west-1' });
 // port on which the server listens
-const port = 80;
+const port = 8080;
 const { v4: uuidv4 } = require('uuid');
 // transforms every input request body
 // into json objects for reading
@@ -28,37 +28,42 @@ app.get("/user/:userId", (req, res) => {
             res.status(500).send("Something went wrong while trying to looking up streaming data")
         }
         else{
-            console.log(streamChecker.getThreshold(data));
-            const params ={
-                KeyConditionExpression: 'userId = :userId',
-                FilterExpression : "contains(#userId, :userId)",
-                ExpressionAttributeValues: {
-                    ':userId': data.Item.userId
-                },
-                ExpressionAttributeNames: {
-                    '#userId': "userId"
-                },
-                TableName: 'user_stream'
-            }
-            client.scan(params, function (err, filterData){
-                if (err){
-                    console.log(err)
-                    res.status(400).send("Error while getting filtered data");
-                }else {
-                    console.log(filterData.Count)
-                    if (filterData.Count > streamChecker.getThreshold(data)){
-                        res.status(429).send("User has exceeded limits");
-                    }else {
-                        res.status(200).send("User has is within limits");
-                    }
+            let user;
+            try{
+                console.log(streamChecker.getThreshold(data));
+                const params ={
 
+                    KeyConditionExpression: 'userId = :userId',
+                    FilterExpression : "contains(#userId, :userId)",
+                    ExpressionAttributeValues: {
+                        ':userId': data.Item.userId
+                    },
+                    ExpressionAttributeNames: {
+                        '#userId': "userId"
+                    },
+                    TableName: 'user_stream'
                 }
-            })
+                client.scan(params, function (err, filterData){
+                    if (err){
+                        console.log("Error while trying to get data")
+                        res.status(400).send("Error while getting filtered data");
+                    }else {
+                        console.log(filterData.Count)
+                        if (filterData.Count > streamChecker.getThreshold(data)){
+                            res.status(429).send(res.json({message:"User has exceeded limits"}));
+                        }else {
+                            res.status(200).send(res.json({message:"User is still within limits"}));
+                        }
+                    }
+                })
+            }catch (err){
+                res.status(400).send(res.json({message: 'User does not exist',id: userId.Key}))
+            }
         }
     });
 });
 app.get('/health', (req, res) => {
-    res.send('Hello World!')
+    res.status(200).send("Services reached");
 })
 const streamChecker = {
     getThreshold: (data) => {
